e743a61b6a470df6c0f7f7a735f8fbb5d5ef9762
Tung No <111445323+tungno@users.noreply.github.com>
2024-11-12 23:57:15 +0100

    deploy8

--- root/modules/app_services/main.tf
+++ root/modules/app_services/main.tf
@@ -15,12 +15,18 @@ resource "azurerm_linux_web_app" "app_service" {
   service_plan_id     = azurerm_service_plan.app_service_plan.id
 
   site_config {
-    linux_fx_version = "DOCKER|${var.docker_image_name}:${var.docker_image_tag}"
+    # Add any additional site configurations here
   }
 
   app_settings = merge(var.app_settings, {
-    "WEBSITES_PORT" = "8080"
+    "WEBSITES_PORT"               = "8080"
+    "DOCKER_CUSTOM_IMAGE_NAME"    = "${var.docker_image_name}:${var.docker_image_tag}"
+    # Optional: Docker registry settings
+    # "DOCKER_REGISTRY_SERVER_URL"      = var.registry_url
+    # "DOCKER_REGISTRY_SERVER_USERNAME" = var.registry_username
+    # "DOCKER_REGISTRY_SERVER_PASSWORD" = var.registry_password
   })
 
   https_only = true
 }
+
